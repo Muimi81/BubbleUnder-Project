7cfccf20bbfdd02ddc1405f469d1390c24de0a4b
Nzangi Muimi <nzangimuimi@gmail.com>
2023-05-30 10:57:58 +0300

    Updates

---
+++ common.js
@@ -0,0 +1,8 @@
+//script for the Bubble Under Project Site
+
+$ (document).ready(fucntion(){
+    $("#contactname").val("Please eneter your full name");
+    $("#telephone").val("Incl local dialling code");
+    $("eventdate").val("Format DDMMYYYY");
+    $("#details").val("The more you can enter here, the less we have to check with you :)");
+});
