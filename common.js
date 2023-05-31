14f79f337bd5a48d4451707bf0a9e3eee282ea7d
Nzangi Muimi <nzangimuimi@gmail.com>
2023-05-31 20:19:33 +0300

    Updates

--- common.js
+++ common.js
@@ -1,8 +1,33 @@
-//script for the Bubble Under Project Site
+//jQuery script for the Bubble Under Project Site
 
-$ (document).ready(fucntion(){
-    $("#contactname").val("Please eneter your full name");
-    $("#telephone").val("Incl local dialling code");
+$ (document).ready(function(){
+
+    //Enhancing the contact form with helper text (code below)
+
+    $("#contactname").val("Please enter your full name");
+
+    $("#telephone").val("Incl. local dialing code");
+
     $("eventdate").val("Format DDMMYYYY");
+
     $("#details").val("The more you can enter here, the less we have to check with you :)");
+
+    $("input, textarea").focus(function(){
+        $(this).select();
+    });
+
+
+    //Making the gallery captions appear on hover only (code below)
+
+    $("figcaption").hide();
+
+    $("figure").each(function(){
+        //adding the behaviour for each figure here
+        $(this).hover(function(){
+            $(this).find("figcaption").slideDown('medium');
+        },function(){
+            $(this).find("figcaption").slideUp('medium');
+        });
+    });
+
 });
